e0f5da666ea8689bbafc819cbd182a15d15c1f85
Daniel Alvarenga <danielalvarengalima@gmail.com>
2014-01-24 11:59:52 -0300

    add google analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,15 +20,3 @@
 $(function() {
   $(document).foundation();
 });
-
-// Início do código do Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-47444418-1', 'listapresentes.com');
-ga('send', 'pageview');
-// Fim do código do Google Analytics
-
-
